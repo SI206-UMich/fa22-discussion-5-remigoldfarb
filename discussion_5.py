350ca17cc04d898a1dc7a853a9d99d1bdd4c67c3
remi goldfarb <remigoldfarb@remis-MacBook-Air.local>
2022-09-28 13:58:59 -0400

    m

--- discussion_5.py
+++ discussion_5.py
@@ -38,22 +38,18 @@ class Warehouse:
 
 	# Adds an item to the warehouse	
 	def add_item(self, item):
+		#self.items.append(item)
 		pass
 
 	# Returns the item in the warehouse with the most stock		
 	def get_max_stock(self):
-		#self.item1 = Item("Beer", 6, 20)
-		#self.item2 = Item("Cider", 5, 25)
-		#self.item3 = Item("Water", 1, 100)
-		#self.item4 = Item("Fanta", 2, 60)
-		Costco = Warehouse([self.item1,self.item2,self.item3])
-		mx_stock = Costco.get_max_stock()
-		self.assertEqual(mx_stock,60)
-
-		Costco.add_item(self.item3)
-		mx_stock_new = Costco.get_max_stock()
-		self.assertEqual(mx_stock_new,self.item3)
-		pass
+		max_stock = 0
+		max_stock_item = None
+		for item in self.items:
+			if item.stock > max_stock:
+				max_stock = item.stock
+				max_stock_item = item
+		return max_stock_item
 	
 	# Returns the item in the warehouse with the highest price
 	def get_max_price(self):
@@ -87,14 +83,25 @@ class TestAllMethods(unittest.TestCase):
 		Target = Warehouse([self.item1,self.item2])
 		Target.add_item(self.item3)
 		Target.add_item(self.item4)
-		self.assertEaual(Target.items,[self.item1, self.item2, self.item3, self.item4])
+		self.assertEqual(Target.items,[self.item1, self.item2, self.item3, self.item4])
 		pass
 
 
 	## Check to see whether warehouse correctly returns the item with the most stock
 	def test_warehouse_max_stocks(self):
-		
-		pass
+		def get_max_stock(self):
+		#self.item1 = Item("Beer", 6, 20)
+		#self.item2 = Item("Cider", 5, 25)
+		#self.item3 = Item("Water", 1, 100)
+		#self.item4 = Item("Fanta", 2, 60)
+			Costco = Warehouse([self.item1,self.item2,self.item4])
+			mx_stock = Costco.get_max_stock()
+			self.assertEqual(mx_stock,self.item4)
+
+			Costco.add_item(self.item3)
+			mx_stock_new = Costco.get_max_stock()
+			self.assertEqual(mx_stock_new,self.item3)
+			pass
 
 
 	# Check to see whether the warehouse correctly return the item with the highest price
